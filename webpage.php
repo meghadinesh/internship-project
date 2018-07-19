7223d1d5ab782497314df48833221f80829ea7d7
Megha Dinesh <meghadinesh3@gmail.com>
2018-07-19 21:55:23 +0530

    Update webpage.php

--- webpage.php
+++ webpage.php
@@ -1,6 +1,7 @@
 <html>
 <head>
   <title>Irrigation for Tomorrow</title>
+  <link href="https://fonts.googleapis.com/css?family=Arvo" rel="stylesheet">
   <style>
   button:hover{
     background-color: rgb(190, 171, 205);
@@ -8,13 +9,14 @@
   }
   h1{
     color: green;
-    font-size: 40px;
-    font-family: sans-serif;
+    font-size: 50px;
+    font-family:'Arvo', serif ;
   }
  body {
+   background-image: url("http://3.bp.blogspot.com/_DziNhZUeF0Y/TTL7o3mZnkI/AAAAAAAAAEg/BRkGhKMrr4s/s1600/PivotWithDrops.JPG");
     background-color: rgb(130, 248, 124);
 }
-#img {
+/*#img {
   display: block;
   margin-left: auto;
   margin-right: auto;
@@ -23,11 +25,15 @@
   border-color: black;
   border-style: hidden;
   border-width: 5px;
-  }
+  }*/
   #but {
     border-radius: 10px;
     width: 100px;
     height: 30px;
+    /*margin: 0 auto;*/
+    position: absolute;
+    top: 70%; left: 45%;
+
   }
 /*  body,html{
     height: 100%;
@@ -40,18 +46,20 @@
 </head>
 <body>
 
-<h1 align="center">Irrigation for tomorrow</h1>
-<img id="img" src="irrg.jpg" alt="Irrigation">
+<h1 align="center">IRRIGATION  FOR  TOMORROW</h1>
+<!--<img id="img" src="irrg.jpg" alt="Irrigation">-->
 <br>
 <br>
 <br>
-<br>
-<p>
-  Irrigation may be defined as the science of artificial application of water to the land in order to fulfill the water requirements of the crops throughout the crop period for the full nourishment of the crops.
+<table align="center" width=50% cellpadding=20% style="background-color:rgba(237, 249, 102, 0.45);">
+<tr>
+<td style="font-size: 20px;">  Irrigation may be defined as the science of artificial application of water to the land in order to fulfill the water requirements of the crops throughout the crop period for the full nourishment of the crops.
   Nutrients to the crops may also be applied through irrigation.
-</p>
+</td></tr>
+</table>
 <form action="status.php">
-<button id="but" type="submit">Check Status</button>
+<button id="but" type="submit" >Check Status</button>
 </form>
 </body>
 </html>
+
